4b53daf7b8daaff45bdb7f3194153d2ffc4fd0ec
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-07-27 23:59:12 -0300

    FEAT : add default files

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,5 +10,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/api', userRoutes);
 
-mongoose.connect(process.env.DB_URI as string).then(()=> app.listen(PORT, () => console.log("Servidor Ativo")))
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParse : true, 
+  useUnifieldTopology : true
+}).then(()=> app.listen(PORT, () => console.log("Servidor Ativo")))
 .catch((error) => console.log(error.message))
